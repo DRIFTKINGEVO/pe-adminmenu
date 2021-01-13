4c4e39ffe1c94f240c8ce90066635e13108a437a
xMontesito <ANDRESMONTEHERMOSOG@gmail.com>
2021-01-13 13:27:09 -0500

    added local fr

---
+++ locales/fr.lua
@@ -0,0 +1,73 @@
+Locales['es'] = {
+
+  --Basic Notifications
+  ['admin_menu']          = '🛡️ | Menu administratif',
+  ['perms_false']         = '🛡️ | Vous navez pas le droit de voir ça',
+  ['user_perms']          = '🛡️ | Vous nêtes pas autorisé à faire cela',
+
+  ['kick_msg']            = 'Ils ont tous été mis en route à cause dun problème de système. Nous espérons que vous reviendrez bientôt',
+  ['ten_min_close']       = '# **Il va y avoir une tempête dans 10 minutes**\n ## Faites attention.\n',
+  ['admin_news']          = 'Communiqué administratif',
+  ['10_min']              = 'Tous les véhicules vides seront supprimés en 10 minutes',
+  ['3_min']               = 'Tous les véhicules vides seront supprimés en 3 minutes',
+  ['30_sec']              = 'Tous les véhicules vides seront supprimés en 30 secondes',
+  ['success_delall']      = '✔️ | Tous les véhicules ont été supprimés avec succès!',
+
+  --Menu
+  ['admin_menu_top']      = 'Menu administratif',
+  ['player_list']         = 'Liste des joueurs',
+  ['server_admin']        = 'Serveur',
+  ['admin_admin']         = 'Admin',
+  ['jugador_admin']       = 'Joueurs',
+  ['extra_admin']         = 'Extras',
+
+  --Server
+  ['del_veh_time']        = 'Supprimer des voitures (10min)',
+  ['del_veh']             = 'Supprimer des voitures (insta)',
+  ['del_obj']             = 'Suppression des objets (insta)',
+  ['del_chat']            = 'Suppression du chat',
+  ['ten_min']             = 'Annonce de la réinitialisation (10min)',
+  ['kick_all']            = 'Expulser tout le monde',
+  ['revive_all']          = 'Ranimer tout le monde',
+  ['custom_announce']     = 'En prog',
+
+  ['player_name']         = 'Nom: %s',
+
+  --Admin
+  ['noclip']              = 'Noclip',
+  ['god']                 = 'Invincible',
+  ['tp']                  = 'Tp au point',
+  ['tpveh']               = 'Tp au véhicule proche',
+  ['spawnCar']            = 'La création dune voiture',
+  ['dv']                  = 'Supprimer la voiture',
+  ['heal']                = 'Cure',
+  ['fix']                 = 'Réparation',
+  ['inv']                 = 'Invisible',
+
+  --Players
+
+  ['freeze']              = 'Geler un lecteur',
+  ['your_rank']           = 'Gamme: %s',
+
+  --Notifications
+  ['noclip_true']         = '✔️ | Vous avez activé le noclip',
+  ['noclip_false']        = '❌ | Vous avez désactivé le noclip',
+  ['god_true']            = '✔️ | Vous avez activé le Godmode',
+  ['god_false']           = '❌ | Vous avez désactivé Godmode',
+  ['inv_true']            = '✔️ | Vous avez activé le mode invisible',
+  ['inv_false']           = '❌ | Vous avez désactivé le mode invisible',
+  ['fix_true']            = '✔️ | Vous avez réparé le véhicule',
+  ['fix_false']           = '❌ | Vous devez être dans un véhicule',
+  ['heal_true']           = '✔️ | Vous êtes guéri',
+  ['tp_true']             = '✔️ | Vous vous êtes téléporté',
+  ['tp_false']            = '❓ | Faire un point',
+  ['veh_wait']            = 'Attends, ne bouge pas....',
+  ['veh_true']            = '✔️ | Vous vous êtes téléporté correctement',
+  ['veh_false']           = '❌ | Aucun véhicule retrouvé',
+  
+  ['delallveh_true']      = '✔️ | Vous avez supprimé toutes les voitures',
+  ['delallobj_true']      = '✔️ | Vous avez supprimé tous les objets ',
+  ['spawn_true']          = 'Vous avez essayé dengendrer un ',
+  ['veh_name']            = 'Indiquez le nom du véhicule correspondant',
+  ['chat_false']          = '💬 | Vous avez supprimé tout le chat'
+}
